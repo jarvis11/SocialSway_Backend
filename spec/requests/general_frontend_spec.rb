b6a3d51aaaf3733048658074cb3133c62be3815e
Bhaskar Ravi <bvravi11@gmail.com>
2014-01-15 00:16:14 -0600

    Added rspec testing unit and implemented tests for general frontend pages

---
+++ spec/requests/general_frontend_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe "General Frontend" do
+  
+  describe "Home" do
+    it "should have the content 'SocialSway is a Self-Service Social Advertising Platform'" do
+      visit '/general_frontend/home'
+      expect(page).to have_content('SocialSway is a Self-Service Social Advertising Platform')
+    end
+  end
+  
+  describe "Time Machine Overview" do
+    it "should have the content 'Time Machine'" do
+      visit '/general_frontend/timemachine_overview'
+      expect(page).to have_content('Time Machine')
+    end
+  end
+  
+  describe "Time Machine Cases" do
+    it "should have the content 'Time Machine in Action'" do
+      visit '/general_frontend/timemachine_cases'
+      expect(page).to have_content('Time Machine in Action')
+    end
+  end
+  
+  describe "Platform" do
+    it "should have the content 'Optimized for Performance'" do
+      visit '/general_frontend/platform'
+      expect(page).to have_content('Optimized for Performance')
+    end
+  end
+  
+  describe "About" do
+    it "should have the content 'Founding Team'" do
+      visit '/general_frontend/about'
+      expect(page).to have_content('Founding Team')
+    end
+  end
+  
+  describe "Contact" do
+    it "should have the content 'Get in touch'" do
+      visit '/general_frontend/contact'
+      expect(page).to have_content('Get in touch')
+    end
+  end
+      
+    
+  
+  
+end
