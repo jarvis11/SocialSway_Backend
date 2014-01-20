051dc08372f724d589b20d9434810a6ee4537a35
Bhaskar Ravi <bvravi11@gmail.com>
2014-01-19 22:47:27 -0600

    modified application.rb for heroku

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module SocialSwayBackend
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.initialize_on_precompile = false
     
     config.assets.paths << Rails.root.join('app', 'assets', 'images', 'Backgrounds')
     
