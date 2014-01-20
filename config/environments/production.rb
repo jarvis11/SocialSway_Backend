2a851fe355366a1a5153ec54cdda783fbfb14f58
Bhaskar Ravi <bvravi11@gmail.com>
2014-01-19 22:41:21 -0600

    Modified prod env once again because this doesn't want to work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ SocialSwayBackend::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true #default is false
+  config.serve_static_assets = false #default is false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true #default is false
+  config.assets.compile = false #default is false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -37,7 +37,7 @@ SocialSwayBackend::Application.configure do
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx, default is commented out
+ # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx, default is commented out
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
